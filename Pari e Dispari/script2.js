87fb8ff80d1583ec5b35c779b688f4df2c69e538
sara luongo <saraluongo202002@gmail.com>
2026-03-31 18:59:39 +0200

    fatte funzioni per varie operazioni e invocate nello scope globale

--- Pari e Dispari/script2.js
+++ Pari e Dispari/script2.js
@@ -4,3 +4,39 @@
 //Sommiamo i due numeri
 //Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 //Dichiariamo chi ha vinto.
+
+const sceltaUtente = prompt('scegli pari o dispari')
+const numeroUtente = prompt('inserisci un numero da 1 a 5')
+
+
+const numeroCasualeComputer = generatoreNumeroRandom()
+
+const totaleNumeri = sommaNumeri(numeroCasualeComputer, numeroUtente)
+
+totaleSommaNumeri(totaleNumeri, sceltaUtente)
+
+
+
+
+function totaleSommaNumeri(somma, scelta) {
+    const vittoriaPari = somma % 2 == 0 && scelta == 'pari'
+    const vittoriaDispari = somma % 2 !== 0 && scelta == 'dispari'
+
+    if (vittoriaPari || vittoriaDispari) {
+        console.log('hai vinto')
+    } else {
+        console.log('hai perso')
+    }
+
+}
+
+
+function generatoreNumeroRandom() {
+    const numeroCasuale = (Math.floor(Math.random) * 5)
+    return numeroCasuale
+
+}
+function sommaNumeri(num1, num2) {
+    return num1 + num2
+
+}
