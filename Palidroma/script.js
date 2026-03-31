c3862f6d22bd62e0b907d1875545bfb0405c9eb4
sara luongo <saraluongo202002@gmail.com>
2026-03-31 16:34:28 +0200

    creata logica per verificare se una parola è palindroma poi aggiunto tutto in una funzione

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -2,3 +2,17 @@
 //Chiedere all’utente di inserire una parola
 //Creare una funzione per capire se la parola inserita è palindroma.
 
+const parolaInserita = prompt('inserisci una parola');
+let parolaPalindroma = ''
+
+function verificaPalindromo(parolaDaVerificare) {
+    for (let i = parolaDaVerificare.length - 1; i >= 0; i--) {
+        parolaPalindroma += parolaDaVerificare[i]
+    }
+    if (parolaDaVerificare === parolaPalindroma) {
+        console.log('la parolaInserita è palindroma')
+    } else {
+        console.log('la parola inserita non è palindroma')
+    }
+}
+verificaPalindromo(parolaInserita);
